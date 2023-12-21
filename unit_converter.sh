5450ccb318fce5ac8bd71e7d83ccf6040db70b62
Waldek <waldek.bus@gmail.com>
2023-12-21 09:01:17 +0100

    Stage 2 of unit_converter

--- unit_converter.sh
+++ unit_converter.sh
@@ -9,16 +9,32 @@ constant="${user_input[1]}"
 
 regex='^[a-zA-Z]+_to_[a-zA-Z]+$'
 
-return_message="none"
-
 if [ "$arr_length" -ne 2 ]; then
-	return_message="The definition is incorrect!"
+	echo "The definition is incorrect!"
+    exit 1
 elif ! [[ $definition =~ $regex ]]; then
-	return_message="The definition is incorrect!"
+	echo "The definition is incorrect!"
+	exit 1
 elif ! [[ $constant =~ ^-?[0-9]+$ || $constant =~ ^-?[0-9]+[.][0-9]+$ ]]; then
-	return_message="The definition is incorrect!"
-else
-	return_message="The definition is correct!"
+	echo "The definition is incorrect!"
+	exit 1
 fi
 	
-echo "$return_message"
+
+echo "Enter a value to convert:"
+
+condition=true
+
+while [ "$condition" == true ]; do
+    
+    read value_to_convert
+	
+    if ! [[ $value_to_convert =~ ^-?[0-9]+$ || $value_to_convert =~ ^-?[0-9]+[.][0-9]+$ ]]; then
+		echo "Enter a float or integer value!"
+	else
+		condition=false
+	fi
+done
+
+result=$(echo "scale=2; $constant * $value_to_convert" | bc -l)
+printf "Result: %s\n" "$result"
