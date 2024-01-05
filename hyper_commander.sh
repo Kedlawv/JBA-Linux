7163ab7f2d0baa4174c70be6965d8d597f9b2ffd
Waldek <waldek.bus@gmail.com>
2024-01-05 12:56:44 +0100

    Stage One hyper_commander

---
+++ hyper_commander.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+
+print_menu() {
+    echo "------------------------------"
+    echo "| Hyper Commander            |"
+    echo "| 0: Exit                    |"
+    echo "| 1: OS info                 |"
+    echo "| 2: User info               |"
+	echo "| 3: File and Dir operations |"
+	echo "| 4: Find Executables        |"
+	echo "------------------------------"
+}
+
+process_user_choice() {
+
+	read user_choice
+	
+	case $user_choice in
+	
+		0)	
+			echo "Farewell!"
+			exit 0
+			;;
+		1)
+			echo "Not implemented!"
+			;;
+		2)
+			echo "Not implemented!"
+			;;
+		3)
+			echo "Not implemented!"
+			;;
+		4)
+			echo "Not implemented!"
+			;;
+		*) 
+			echo "Invalid option!"
+			;;
+	esac
+}
+
+echo "Hello $USER!"
+
+while [ true ]; do
+	
+	
+	print_menu
+	process_user_choice
+done
+
+
